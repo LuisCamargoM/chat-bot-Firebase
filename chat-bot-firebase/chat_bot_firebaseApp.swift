17d379c685ebbf301a7e6399b7859b54f4d89c27
Luis Camargo <luis.e.c.mendoza@gmail.comqqqqqq>
2022-12-28 22:09:31 -0300

    Initial Commit

---
+++ chat-bot-firebase/chat_bot_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_bot_firebaseApp.swift
+//  chat-bot-firebase
+//
+//  Created by Luis Camargo on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct chat_bot_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
